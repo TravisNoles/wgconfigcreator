46857bb284f4c4ff83696468b85a847a2a296a9d
travis <larupia@gmail.com>
2020-08-21 16:17:37 -0500

    added cli group commands

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Wireguard Configuration Creator
 import fire
-from wgconfigeditor import WGConfigEditor
+from wgconfigeditor import WGConfig
 
 class WireguardServer(object):
     def new(self, ipaddress, interface_name):
@@ -11,11 +11,23 @@ class WireguardServer(object):
         new_wg.add_server_interface(new_wg.publickey, ipaddress)
 
 
+class WireguardClient(object):
+    def __init__(self):
+        WireguardServer()
 
+    def new(self):
+        """ Create new wireguard client config. """
+        print('test')
+
+
+class Pipeline(object):
+    def __init__(self):
+        self.server = WireguardServer()
+        self.client = WireguardClient()
 
 
 
 
 
 if __name__ == '__main__':
-    fire.Fire(WireguardServer)
+    fire.Fire(Pipeline)
